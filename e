002f572279236dbe4fe6931ee2271ce9b56c3ebb
IMaxTheGoat <isaac@naraimax.com>
2022-02-09 22:14:32 -0700

    created board, initialize board, and fill in board with rand. not all rands are  in the right place. maybe make 9 instances of each number and place it in a random place, while it is in the same column, row, or block of that number keep choosing

---
+++ e
@@ -0,0 +1,14 @@
+  Connect-4[m
+  TicTacToeAI[m
+  master[m
+* [32mminesweeper[m
+  [31mremotes/origin/Battleship[m
+  [31mremotes/origin/Connect-4[m
+  [31mremotes/origin/HEAD[m -> origin/master
+  [31mremotes/origin/Tic-Tac-Toe(PvP)[m
+  [31mremotes/origin/Tic-Tac-Toe(Random)[m
+  [31mremotes/origin/TicTacToeAI[m
+  [31mremotes/origin/UNO[m
+  [31mremotes/origin/master[m
+  [31mremotes/origin/minesweeper[m
+  [31mremotes/origin/sudoku[m
--- sudoku-code.cpp
+++ sudoku-code.cpp
@@ -1,8 +1,134 @@
 #include <iostream>
 #include <stdlib.h>
+#include <vector>
 
 using namespace std;
 
+int difficultyLevel = 15;
+
+string line = " +";
+char empty = ' ';
+
+int boardInt[9][9];
+char boardBP[9][9];
+char boardCell[9][9] = {
+                        {empty,empty,empty,empty,empty,empty,empty,empty,empty},
+                        {empty,empty,empty,empty,empty,empty,empty,empty,empty},
+                        {empty,empty,empty,empty,empty,empty,empty,empty,empty},
+                        
+                        {empty,empty,empty,empty,empty,empty,empty,empty,empty},
+                        {empty,empty,empty,empty,empty,empty,empty,empty,empty},
+                        {empty,empty,empty,empty,empty,empty,empty,empty,empty},
+
+                        {empty,empty,empty,empty,empty,empty,empty,empty,empty},
+                        {empty,empty,empty,empty,empty,empty,empty,empty,empty},
+                        {empty,empty,empty,empty,empty,empty,empty,empty,empty}
+};
+
+void repeatPrint(string str, int itr){
+    for (int i = 0; i < itr; i++){
+        cout << str;
+    }
+}
+
+void fillBoard(){
+    srand(time(NULL));
+
+    for (int y = 0; y < 9; y++){
+        for (int x = 0; x < 9; x++){
+            boardInt[y][x] = rand() % 9 + 1;
+        }
+    }
+}
+
+void initializeBoard(){
+    for (int y = 0; y < 9; y++){
+        for (int x = 0; x < 9; x++){
+            switch (boardInt[y][x]){
+                case 1:
+                    boardBP[y][x] = '1';
+                    break;
+                case 2:
+                    boardBP[y][x] = '2';
+                    break;
+                case 3:
+                    boardBP[y][x] = '3';
+                    break;
+                case 4:
+                    boardBP[y][x] = '4';
+                    break;
+                case 5:
+                    boardBP[y][x] = '5';
+                    break;
+                case 6:
+                    boardBP[y][x] = '6';
+                    break;
+                case 7:
+                    boardBP[y][x] = '7';
+                    break;
+                case 8:
+                    boardBP[y][x] = '8';
+                    break;
+                case 9:
+                    boardBP[y][x] = '9';
+                    break;
+                default:
+                    boardBP[y][x] = '0';
+                    break;
+            }
+        }
+    }
+
+    for (int y = 0; y < 9; y++){
+        for (int x = 0; x < 9; x++){
+            if (rand() % 100 < difficultyLevel){
+                boardCell[y][x] = boardBP[y][x];
+            }
+        }
+    }
+}
+
+void drawBoard(){
+
+    int columnLine = 3;
+    int rowLine = 4;
+    int yCoord = 9;
+
+    for (int y = 0; y < 9; y++){
+
+        if (rowLine == 4){
+            cout << ' ';
+            repeatPrint(line,13);
+            cout << endl;
+            rowLine = 1;
+        }
+        if (rowLine != 4){
+            cout << yCoord;
+        }
+
+        for (int x = 0; x < 9; x++){
+            if (columnLine == 3){
+                cout << line;
+                columnLine = 0;
+            }
+            cout << ' ' << boardCell[y][x];
+            columnLine++;
+        }
+
+        cout << line << endl;
+        rowLine++;
+        yCoord--;
+    }
+
+    cout << ' ';
+    repeatPrint(line,13);
+    cout << endl;
+    cout << "    1 2 3   4 5 6   7 8 9\n";
+
+}
+
 int main(){
-    
+    fillBoard();
+    initializeBoard();
+    drawBoard();    
 }
